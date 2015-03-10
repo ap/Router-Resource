35b0b387f3897fd64e9089f1407f55837cfba749
David E. Wheeler <david@justatheory.com>
2015-03-10 10:34:01 -0700

    Increment to v0.21.

--- lib/Router/Resource.pm
+++ lib/Router/Resource.pm
@@ -8,7 +8,7 @@ use Sub::Exporter -setup => {
     groups  => { default => [ qw(resource router missing GET POST PUT DELETE HEAD OPTIONS TRACE CONNECT) ] }
 };
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 sub new {
     my $class = shift;
